58edac85b4f954d749f6c14bf196907e7a2544a7
Richie <richiecontrolsystems@outlook.com>
2023-09-16 16:41:05 +0100

    Posts test search subreddit display correct

--- src/features/Posts/__test__/Posts.test.js
+++ src/features/Posts/__test__/Posts.test.js
@@ -7,7 +7,9 @@ import { Posts } from '../Posts';
 
 const mockStore = configureStore([]);
 
-test('renders Subreddit', () => {
+
+
+test('renders Subreddit when searchTerm is empty', () => {
   const initialState = {
     posts: {
       posts: {
@@ -36,6 +38,41 @@ test('renders Subreddit', () => {
     </Provider>
   );
 
-  expect(getByText(/Subreddit/i)).toBeInTheDocument();
+  expect(getByText('Subreddit: r/pics')).toBeInTheDocument();
+});
+
+
+
+test('renders Searchterm when searchTerm is not empty', () => {
+  const initialState = {
+    posts: {
+      posts: {
+        data: {
+          children: [
+            { data: { id: '1', title: 'Post 1', selftext: 'Content 1', url: 'url1', author: 'Author 1' } },
+            { data: { id: '2', title: 'Post 2', selftext: 'Content 2', url: 'url2', author: 'Author 2' } },
+            // Add more posts as needed
+          ],
+        },
+      },
+      searchTerm: 'YourSearchTermHere', // Set a non-empty searchTerm
+      selectedSubreddit: 'r/pics',
+    },
+    comments: {
+      commentsButtonsDisplay: 'Show Comments',
+    },
+  };
+  const store = mockStore(initialState);
+
+  const { getByText } = render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <Posts />
+      </MemoryRouter>
+    </Provider>
+  );
+
+  // Check if the text "Searchterm: YourSearchTermHere" is present in the component
+  expect(getByText('Searchterm: YourSearchTermHere')).toBeInTheDocument();
 });
 
