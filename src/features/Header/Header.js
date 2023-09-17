e55b5b31c00ae9953cb4cde386b70612e6524f70
Richie <richiecontrolsystems@outlook.com>
2023-09-17 16:15:18 +0100

    Link to Posts page fixed when enter/go key pressed

--- src/features/Header/Header.js
+++ src/features/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { HiOutlineSearch } from 'react-icons/hi';
@@ -12,6 +12,7 @@ const Header = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const dispatch = useDispatch();
   const searchItem = useSelector((state) => state.posts.searchTerm);
+  const linkRef = useRef();
   
   const onSearchChanged = e => setSearchTerm(e.target.value)
 
@@ -25,6 +26,9 @@ const Header = () => {
     if (e.key === 'Enter') {
       e.preventDefault(); // Prevent form submission
       onSearchClicked();
+      if (linkRef.current) {
+        linkRef.current.click(); // Programmatically trigger the Link click event
+      }
     }
   };
   
@@ -49,14 +53,13 @@ const Header = () => {
           value={searchTerm}      
           onChange={onSearchChanged}
           onKeyDown={handleEnterKeyPress}/>
-        <Link 
-        to="/" >  
+        <Link to="/" ref={linkRef}>  
           <button
             className="button" 
             type="button" 
             aria-label="Search"
             onClick={onSearchClicked}>
-            <HiOutlineSearch/>
+              <HiOutlineSearch/>
           </button>
         </Link>
       </form>
